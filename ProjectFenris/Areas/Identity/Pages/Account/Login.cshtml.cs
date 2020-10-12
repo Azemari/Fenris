88e7f09c534d6ff79265be9432330a274d6dc78d
Sam Bennett <sam.bennett1998@hotmail.com>
2020-10-12 21:26:26 +0100

    Styled account related pages Created a shell Welcome Page

--- ProjectFenris/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ProjectFenris/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -96,7 +96,7 @@ namespace ProjectFenris.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Incorrect username or password");
                     return Page();
                 }
             }
